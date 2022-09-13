d70a3958405cdb833322aa3904a14ae7f1e32037
yuanjie <yuanjie@xtourtec.com>
2022-09-13 18:42:14 +0800

    the Second

--- app/src/main/java/com/example/databindingstudy/MainActivity.java
+++ app/src/main/java/com/example/databindingstudy/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.databindingstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.databindingstudy.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding =ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
